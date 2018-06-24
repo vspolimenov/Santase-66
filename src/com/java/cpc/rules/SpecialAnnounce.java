8a89544124db31b77e0f3d20113083f2ea02c48f
Vasilen Polimenov <vspolimenov@dir.bg>
2018-06-24 20:05:46 +0300

    Add files via upload
    Refactor special announces

--- src/com/java/cpc/rules/SpecialAnnounce.java
+++ src/com/java/cpc/rules/SpecialAnnounce.java
@@ -8,20 +8,10 @@ package com.java.cpc.rules;
  *
  */
 public enum SpecialAnnounce {
-	Twenty(20), Fourty(20);
+	Twenty(20), Fourty(40);
 	
 	public int points;
 	SpecialAnnounce(int points) {
 		this.points = points;
 	}
 }
-
-// A tierce — a sequence of three (sequences are in the "A K Q J 10 9 8 7" order
-// of the same suit) — is worth 20 points
-// A quarte — a sequence of four — is worth 50 points.
-// A quint — a sequence of five - is worth 100 points (longer sequences are not
-// awarded, a sequence of eight is counted as a quint plus a tierce)
-// A carré of Jacks is worth 200 points.
-// A carré of Nines is worth 150 points.
-// A carré of Aces, Kings, Queens, or Tens is worth 100 points. (Sevens and
-// Eights are not awarded
