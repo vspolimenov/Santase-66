b568dea25420de0d52f1f0d61411a18485c701c8
Vasilen Polimenov <vspolimenov@dir.bg>
2018-06-24 15:02:01 +0300

    Add files via upload
    Removing logic for announces

--- src/com/java/cpc/io/ConsoleIOManager.java
+++ src/com/java/cpc/io/ConsoleIOManager.java
@@ -18,34 +18,6 @@ public class ConsoleIOManager {
 		return sc.nextInt() - 1;
 	}
 
-	public static int getPlayerAnnounceNumber(int last) {
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Now it is your turn! Make announce(1-9):");
-		if (last <= 1) {
-			System.out.println("1) Clubs.");
-		}
-		if (last <= 2) {
-			System.out.println("2) Dyamonds.");
-		}
-		if (last <= 3) {
-			System.out.println("3) Hearts.");
-		}
-		if (last <= 4) {
-			System.out.println("4) Spades.");
-		}
-		if (last <= 5) {
-			System.out.println("5) NoTrump.");
-		}
-		if (last <= 6) {
-			System.out.println("6) AllTrumps.");
-		}
-		if (last != 0) {
-			System.out.println("7) Contra.");
-		} else if (last == 7) {
-			System.out.println("8) ReContra.");
-		}
-
-		return sc.nextInt();
-	}
+	
 
 }
